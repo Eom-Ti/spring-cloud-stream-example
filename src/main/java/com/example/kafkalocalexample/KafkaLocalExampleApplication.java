dc6d4d033b243094e805900598bea9e4a7ce4431
엄태권 <akfls221@gmail.com>
2024-05-05 22:26:33 +0900

    Initial commit

---
+++ src/main/java/com/example/kafkalocalexample/KafkaLocalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkalocalexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLocalExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaLocalExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkalocalexample/KafkaLocalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkalocalexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLocalExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
