ec9804a197509288e8e634c242c806c9029fec21
엄태권 <akfls221@gmail.com>
2024-05-16 03:51:35 +0900

    fix : default configuration add on properties file

--- producer/src/main/java/com/example/exrate/client/properties/KrEximProperties.java
+++ producer/src/main/java/com/example/exrate/client/properties/KrEximProperties.java
@@ -9,6 +9,11 @@ public record KrEximProperties(
         String authKey
 ) {
 
+    public static final String AUTH_KEY = "authkey";
+    public static final String SEARCH_DATE = "searchdate";
+    public static final String DATA = "data";
+    public static final String EXRATE_TYPE = "AP01";
+
     public String getRequestExrateUrl() {
         return baseUrl() + exrateUrl();
     }
