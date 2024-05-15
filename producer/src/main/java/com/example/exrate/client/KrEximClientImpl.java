21beb098ffef860d104ff02a3715541ab5d36b4e
엄태권 <akfls221@gmail.com>
2024-05-16 03:52:11 +0900

    fix : fix KrEximClientImpl class

--- producer/src/main/java/com/example/exrate/client/KrEximClientImpl.java
+++ producer/src/main/java/com/example/exrate/client/KrEximClientImpl.java
@@ -1,6 +1,5 @@
 package com.example.exrate.client;
 
-import com.example.exrate.client.data.ExrateDataResult;
 import com.example.exrate.client.properties.KrEximProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,36 +8,44 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class KrEximClientImpl implements ExrateApiClient {
-
-    private static final String EXRATE_TYPE = "AP01";
-
     private final RestTemplate restTemplate;
     private final KrEximProperties krEximProperties;
+    private final String searchDate = createSearchDate();
 
     @Override
-    public ExrateDataResult getExrate() {
+    public String getExrate() {
         try {
             log.info("[ExrateApiClient] Start getExrate");
 
-            String requestExrateUrl = krEximProperties.getRequestExrateUrl();
+            String requestExrateUrl = this.krEximProperties.getRequestExrateUrl();
 
             URI uri = UriComponentsBuilder.fromUriString(requestExrateUrl)
-                    .queryParam("authkey", krEximProperties.authKey())
-                    .queryParam("searchdate", EXRATE_TYPE)
+                    .queryParam(KrEximProperties.AUTH_KEY, this.krEximProperties.authKey())
+                    .queryParam(KrEximProperties.SEARCH_DATE, this.searchDate)
+                    .queryParam(KrEximProperties.DATA, KrEximProperties.EXRATE_TYPE)
                     .build().toUri();
 
-            ExrateDataResult exrateDataResult = restTemplate.getForObject(uri, ExrateDataResult.class);
+            String response = this.restTemplate.getForObject(uri, String.class);
 
-            log.info("[ExrateApiClient] End getExrate exrateData : {}", exrateDataResult);
-            return exrateDataResult;
+            log.info("[ExrateApiClient] End getExrate exrateData : {}", response);
+            return response;
         } catch (Exception e) {
             log.error("[ExrateApiClient] has Exception", e);
             throw new RuntimeException("call Api Fail");
         }
+    }
+
+    private String createSearchDate() {
+        ZonedDateTime now = ZonedDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        return now.format(formatter);
     }
 }
