2034e08611ea75e6e40b4701bd3abb7d2d00fb18
엄태권 <akfls221@gmail.com>
2024-05-23 03:12:52 +0900

    feat : change RestTemplateConfig to RestClientConfiguration

---
+++ producer/src/main/java/com/example/coin/config/RestClientConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.coin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfiguration {
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder()
+                .requestFactory(new HttpComponentsClientHttpRequestFactory())
+                .defaultHeader("accept", "application/json")
+                .build();
+    }
+}
