fca45b19a7ba9f296c818efaaf186af94673ed9c
엄태권 <akfls221@gmail.com>
2024-05-26 01:32:45 +0900

    fix : fix coinApiClient interface

--- producer/src/main/java/com/example/coin/client/CoinApiClient.java
+++ producer/src/main/java/com/example/coin/client/CoinApiClient.java
@@ -1,12 +1,10 @@
 package com.example.coin.client;
 
-import com.example.coin.client.data.MarketCode;
-
 import java.util.List;
 
 public interface CoinApiClient {
 
     String getCandlesByMinute(String marketCode);
 
-    List<MarketCode> getMarketCodes();
+    List<String> getMarketCodes();
 }
