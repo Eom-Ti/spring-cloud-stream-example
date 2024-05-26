6f8d08042fbbdcc2b4415eca37530a086a96dc1c
= <akfls221@gmail.com>
2024-05-26 22:50:15 +0900

    fix : change transformer class file

---
+++ coin-functions/src/main/java/com/example/coin/functions/TransformCoinCandleData.java
@@ -0,0 +1,30 @@
+package com.example.coin.functions;
+
+import com.example.coin.data.Candle;
+import com.example.coin.data.CandleDetail;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class TransformCoinCandleData {
+    private TransformCoinCandleData() {
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(TransformCoinCandleData.class);
+
+    public static List<CandleDetail> toData(String rawData, ExchangeType exchangeType) {
+        log.info("[TransformCoinData.toData] exchangeType : {}, rawData: {}", exchangeType, rawData);
+
+        ObjectMapper objectMapper = exchangeType.getObjectMapper();
+        try {
+            return objectMapper.readValue(rawData, new TypeReference<>() {});
+        } catch (JsonProcessingException e) {
+            log.error("[exrate-functions][TransformExrateData] faile readValue", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
