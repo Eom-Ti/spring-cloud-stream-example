5b3a2b10f161df4dc956150b031deb262aa4f88e
엄태권 <akfls221@gmail.com>
2024-05-30 03:24:49 +0900

    fix : change class to record

--- coin-functions/src/main/java/com/example/coin/data/Candle.java
+++ coin-functions/src/main/java/com/example/coin/data/Candle.java
@@ -2,18 +2,7 @@ package com.example.coin.data;
 
 import java.util.List;
 
-public class Candle {
-
-    private final List<CandleDetail> candleDetails;
-
-    public Candle(List<CandleDetail> candleDetails) {
-        this.candleDetails = candleDetails;
-    }
-
-    @Override
-    public String toString() {
-        return "Candle{" +
-                "candleDetails=" + candleDetails +
-                '}';
-    }
+public record Candle(
+        List<CandleDetail> candleDetails
+) {
 }
--- coin-functions/src/main/java/com/example/coin/data/CandleDetail.java
+++ coin-functions/src/main/java/com/example/coin/data/CandleDetail.java
@@ -3,26 +3,9 @@ package com.example.coin.data;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
-public class CandleDetail {
-
-    private final String market;
-
-    private final BigDecimal price;
-
-    private final ZonedDateTime candleTime;
-
-    public CandleDetail(String market, BigDecimal price, ZonedDateTime candleTime) {
-        this.market = market;
-        this.price = price;
-        this.candleTime = candleTime;
-    }
-
-    @Override
-    public String toString() {
-        return "CandleDetail{" +
-                "market='" + market + '\'' +
-                ", price=" + price +
-                ", candleTime=" + candleTime +
-                '}';
-    }
+public record CandleDetail(
+        String market,
+        BigDecimal price,
+        ZonedDateTime candleTime
+) {
 }
