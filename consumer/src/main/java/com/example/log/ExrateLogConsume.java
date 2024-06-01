7a6a4c167f3c2784cd7b0992ae46bea2e73c0a0b
엄태권 <akfls221@gmail.com>
2024-06-01 17:33:41 +0900

    feat : just save

--- consumer/src/main/java/com/example/log/ExrateLogConsume.java
+++ consumer/src/main/java/com/example/log/ExrateLogConsume.java
@@ -1,16 +1,27 @@
 package com.example.log;
 
+import com.example.coin.data.Candle;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Configuration
 public class ExrateLogConsume {
 
     @Bean
-    public Consumer<List<String>> log() {
-        return exRates -> System.out.println("exRates = " + exRates);
+    public Function<List<Candle>, String> generateReport() {
+        return test -> "dd";
+    }
+
+    @Bean
+    @ConditionalOnProperty(value="spring.cloud.function.definition", havingValue = "generateReport|log")
+    public Consumer<String> log() {
+        return test -> {
+            System.out.println("test = " + test);
+        };
     }
 }
