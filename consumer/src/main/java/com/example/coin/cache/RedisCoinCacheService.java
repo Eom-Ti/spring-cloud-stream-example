429dbc604beffbfaa1aad0d9cc9bc8fdc3ec8d36
엄태권 <akfls221@gmail.com>
2024-06-03 03:29:58 +0900

    feat : add Redis cacheService class

--- consumer/src/main/java/com/example/coin/cache/RedisCoinCacheService.java
+++ consumer/src/main/java/com/example/coin/cache/RedisCoinCacheService.java
@@ -2,25 +2,37 @@ package com.example.coin.cache;
 
 import com.example.coin.data.Candle;
 import com.example.coin.data.CandleDetail;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
 public class RedisCoinCacheService implements CoinCacheService {
 
-    private final RedisTemplate<String, CandleDetail> redisTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
 
     @Override
-    public List<Candle> put(List<Candle> candles) {
+    public void put(List<Candle> candles) {
         candles.forEach(candle -> candle.candleDetails().forEach(candleDetail -> {
             String market = candleDetail.market();
-            redisTemplate.opsForValue().set(market, candleDetail);
+            redisTemplate.opsForValue().set(market, candleDetail, 20000, TimeUnit.MILLISECONDS);
         }));
+    }
 
-        return candles;
+    @Override
+    public CandleDetail get(CandleDetail candleDetail) {
+        Object cacheResult = redisTemplate.opsForValue().get(candleDetail.market());
+
+        if(cacheResult == null) {
+            return candleDetail;
+        }
+
+        return objectMapper.convertValue(cacheResult, CandleDetail.class);
     }
 }
