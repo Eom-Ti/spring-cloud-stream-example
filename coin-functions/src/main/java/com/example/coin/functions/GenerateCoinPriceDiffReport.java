d9119435c58298458e8a7a9419b5befba39de1d5
엄태권 <akfls221@gmail.com>
2024-06-03 03:30:53 +0900

    feat : add generateCoinPriceDiffReport file

--- coin-functions/src/main/java/com/example/coin/functions/GenerateCoinPriceDiffReport.java
+++ coin-functions/src/main/java/com/example/coin/functions/GenerateCoinPriceDiffReport.java
@@ -1,12 +1,35 @@
 package com.example.coin.functions;
 
-import com.example.coin.data.Candle;
+import com.example.coin.functions.log.data.CoinPriceDiff;
 
 import java.util.List;
 
 public class GenerateCoinPriceDiffReport {
 
-    public static String generateCoinPriceDiffReport(List<Candle> current, List<Candle> old) {
+    private static final String BLUE_DECREASE_SYMBOL = "\u001B[34m\u25BC\u001B[0m";
+    private static final String RED_INCREASE_SYMBOL = "\u001B[31m\u25B2\u001B[0m";
+    private static final String SAME_SYMBOL = "--";
 
+    public static String generateCoinPriceDiffReport(List<CoinPriceDiff> coinPriceDiffs) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (CoinPriceDiff coinPriceDiff : coinPriceDiffs) {
+            stringBuilder.append(coinPriceDiff.market())
+                    .append(getSymbol(coinPriceDiff))
+                    .append(coinPriceDiff.diff())
+                    .append(coinPriceDiff.candleTime())
+                    .append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String getSymbol(CoinPriceDiff coinPriceDiff) {
+        if (coinPriceDiff.isSame()) {
+            return SAME_SYMBOL;
+        } else if (coinPriceDiff.isIncrease()) {
+            return RED_INCREASE_SYMBOL;
+        } else {
+            return BLUE_DECREASE_SYMBOL;
+        }
     }
 }
