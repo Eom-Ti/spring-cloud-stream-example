b0f6c51657c8415cae26bb9f44f05692cd09ab08
owen <akfls221@gmail.com>
2024-06-02 21:22:13 +0900

    feat : save

--- consumer/src/main/java/com/example/coin/log/ExrateLogConsume.java
+++ consumer/src/main/java/com/example/coin/log/ExrateLogConsume.java
@@ -1,6 +1,9 @@
-package com.example.log;
+package com.example.coin.log;
 
+import com.example.coin.cache.CoinCacheService;
 import com.example.coin.data.Candle;
+import io.lettuce.core.api.push.PushListener;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,18 +13,22 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 @Configuration
+@RequiredArgsConstructor
 public class ExrateLogConsume {
 
+    private final CoinCacheService coinCacheService;
+
     @Bean
-    public Function<List<Candle>, String> generateReport() {
-        return test -> "dd";
+    public Function<List<Candle>, List<Candle>> generateReport() {
+
+        return coinCacheService::put;
     }
 
     @Bean
     @ConditionalOnProperty(value="spring.cloud.function.definition", havingValue = "generateReport|log")
     public Consumer<String> log() {
         return test -> {
-            System.out.println("test = " + test);
+            System.out.println("test2 = " + test);
         };
     }
 }
