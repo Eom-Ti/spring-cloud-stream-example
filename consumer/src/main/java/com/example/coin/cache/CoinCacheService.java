d1469d852c7ce3a41e68152eceb1f36fefd5d09c
엄태권 <akfls221@gmail.com>
2024-06-03 03:29:25 +0900

    feat : add cacheservice interface

--- consumer/src/main/java/com/example/coin/cache/CoinCacheService.java
+++ consumer/src/main/java/com/example/coin/cache/CoinCacheService.java
@@ -1,10 +1,13 @@
 package com.example.coin.cache;
 
 import com.example.coin.data.Candle;
+import com.example.coin.data.CandleDetail;
 
 import java.util.List;
 
 public interface CoinCacheService {
 
-    List<Candle> put(List<Candle> candles);
+    void put(List<Candle> candles);
+
+    CandleDetail get(CandleDetail candleDetail);
 }
