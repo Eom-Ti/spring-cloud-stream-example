dfd74da178ef384b2e5760720761498af65fbe18
엄태권 <akfls221@gmail.com>
2024-06-03 03:28:17 +0900

    feat : add record file coinPriceDiff data

---
+++ coin-functions/src/main/java/com/example/coin/functions/log/data/CoinPriceDiff.java
@@ -0,0 +1,18 @@
+package com.example.coin.functions.log.data;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+public record CoinPriceDiff(
+        String market,
+        BigDecimal diff,
+        ZonedDateTime candleTime
+) {
+    public boolean isIncrease() {
+        return diff.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean isSame() {
+        return diff.equals(BigDecimal.ZERO);
+    }
+}
