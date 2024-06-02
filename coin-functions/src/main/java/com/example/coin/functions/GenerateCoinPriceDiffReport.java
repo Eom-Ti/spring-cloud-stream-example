8357d1a8d89f92b00c79d1ea17101d02a832c208
owen <akfls221@gmail.com>
2024-06-02 21:22:30 +0900

    feat : add diff report class file

---
+++ coin-functions/src/main/java/com/example/coin/functions/GenerateCoinPriceDiffReport.java
@@ -0,0 +1,12 @@
+package com.example.coin.functions;
+
+import com.example.coin.data.Candle;
+
+import java.util.List;
+
+public class GenerateCoinPriceDiffReport {
+
+    public static String generateCoinPriceDiffReport(List<Candle> current, List<Candle> old) {
+
+    }
+}
