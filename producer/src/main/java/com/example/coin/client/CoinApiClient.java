5c2452cf11b295288028eff2bcc4f76cfe6a3594
owen <akfls221@gmail.com>
2024-06-04 19:42:33 +0900

    feat : add interface Notes

--- producer/src/main/java/com/example/coin/client/CoinApiClient.java
+++ producer/src/main/java/com/example/coin/client/CoinApiClient.java
@@ -4,7 +4,18 @@ import java.util.List;
 
 public interface CoinApiClient {
 
+    /**
+     * Gets coin price information in fractions.
+     *
+     * @param marketCode marketCode ex)KRW-ETC
+     * @return  String Type CandleData
+     */
     String getCandlesByMinute(String marketCode);
 
+    /**
+     * Gets the market code provided by the current exchange.
+     *
+     * @return List<String> marketCodes
+     */
     List<String> getMarketCodes();
 }
