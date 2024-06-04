503992d17e797b42e465688cf92355991a2c30c8
owen <akfls221@gmail.com>
2024-06-04 19:42:36 +0900

    feat : add interface Notes

--- consumer/src/main/java/com/example/coin/cache/CoinCacheService.java
+++ consumer/src/main/java/com/example/coin/cache/CoinCacheService.java
@@ -7,7 +7,18 @@ import java.util.List;
 
 public interface CoinCacheService {
 
+    /**
+     * Performs the role of storing coin information in the cache.
+     *
+     * @param candles candleDetail List data
+     */
     void put(List<Candle> candles);
 
+    /**
+     * Retrieves stored coin information from the cache. If it does not exist, returns the candleDetail entered.
+     *
+     * @param candleDetail  candleDetail data(market, price, time)
+     * @return  CandleDetail
+     */
     CandleDetail get(CandleDetail candleDetail);
 }
