e300f0fca006d60b3e08670990d751ac1b035ec3
엄태권 <akfls221@gmail.com>
2024-06-07 02:22:05 +0900

    feat : implements Serializable interface

--- coin-functions/src/main/java/com/example/coin/data/message/Candle.java
+++ coin-functions/src/main/java/com/example/coin/data/message/Candle.java
@@ -1,8 +1,9 @@
 package com.example.coin.data.message;
 
+import java.io.Serializable;
 import java.util.List;
 
 public record Candle(
         List<CandleDetail> candleDetails
-) {
+) implements Serializable {
 }
--- coin-functions/src/main/java/com/example/coin/data/message/CandleDetail.java
+++ coin-functions/src/main/java/com/example/coin/data/message/CandleDetail.java
@@ -1,5 +1,6 @@
 package com.example.coin.data.message;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
@@ -7,7 +8,7 @@ public record CandleDetail(
         String market,
         BigDecimal price,
         ZonedDateTime candleTime
-) {
+) implements Serializable  {
     public BigDecimal calcPriceDiff(CandleDetail candleDetail) {
         if (!market.equals(candleDetail.market)) {
             throw new IllegalArgumentException("market is not equal market");
@@ -15,5 +16,4 @@ public record CandleDetail(
 
         return price.subtract(candleDetail.price);
     }
-
 }
